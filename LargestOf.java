9e4983231da3da908d3d3cf62129c8943ee527f1
shri340 <shriharisatpute1234@gmail.com>
2026-08-18 09:11:34 +0530

    Refactor largest number logic and output format

--- LargestOf.java
+++ LargestOf.java
@@ -2,23 +2,26 @@ import java.util.*;
 public class LargestOf {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter the first number: ");
+        System.out.println("Enter the first number: ");
         int a= sc.nextInt();
-        System.out.print("Enter the second number: ");
+        System.out.println("Enter the second number: ");
         int b = sc.nextInt();
-        System.out.print("Enter the third number: ");
+        System.out.println("Enter the third number: ");
         int c = sc.nextInt();
 
-        int largest = a;
+      
 
-        if (b > largest) {
-            largest = b;
+        if (a>b&&a>c) {
+           System.out.println("The largest number is: " + a); 
+        }
+        else if (b>a&&b>c) {
+            System.out.println("The largest number is: " + b); 
+        }
+        else {
+            System.out.println("The largest number is: " + c); 
         }
 
-        if (c > largest) {
-            largest = c;
-        }
 
-        System.out.println("The largest number is: " + largest);
     }
 }
+
