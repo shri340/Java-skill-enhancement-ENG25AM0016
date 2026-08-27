d89c6644f3cde571de2b0c2ab880b4e1ad8d6354
Shrihari Satpute <shriharisatpute1234@gmail.com>
2026-08-27 14:37:09 +0530

    Add files via upload

---
+++ vowelString.java
@@ -0,0 +1,21 @@
+import java.util.*;
+    public class vowelString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (vowels.indexOf(ch) != -1) {
+                count++;
+            }
+        }
+
+        System.out.println("Number of vowels: " + count);
+   sc.close(); }   
+}
